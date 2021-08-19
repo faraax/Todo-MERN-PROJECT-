11e66801c92237fcb7b00f11ce1027006200dce0
unknown <fiverr.maxconner@gmail.comclear>
2021-08-19 20:59:24 +0500

    fist commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/', require('./Route/Route'));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'))
+    app.use(express.static('client/build'))
 }
 
 
